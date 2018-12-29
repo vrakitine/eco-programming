ebed04df43f39f05597f5d662de2c01c89b66753
Val Rakitine <val@natmatch.com>
2018-12-28 21:36:28 -0500

    first working version

--- fsm_p_01_main.py
+++ fsm_p_01_main.py
@@ -1,6 +1,6 @@
 import json
 # init block
-real_number = str("+38t.7")
+real_number = str("+38.7")
 integer_part = str("")
 decimal_part = str("")
 current_state = str("s_00")
@@ -8,7 +8,39 @@ event = str("unknown")
 fsm_matrix = {
     "s_00":{
         "v_10":"s_10",
-    }
+        "v_20":"s_10",
+        "v_30":"s_10",
+        "v_40":"Err_10",
+        "v_50":"Err_20",
+   },    
+    "s_10":{
+        "v_10":"Err_30",
+        "v_20":"s_20",
+        "v_30":"s_30",
+        "v_40":"Err_40",
+        "v_50":"Err_20",
+   },    
+    "s_20":{
+        "v_10":"Err_50",
+        "v_20":"s_20",
+        "v_30":"s_30",
+        "v_40":"END",
+        "v_50":"Err_20",
+   },    
+    "s_30":{
+        "v_10":"Err_60",
+        "v_20":"s_40",
+        "v_30":"Err_70",
+        "v_40":"END",
+        "v_50":"Err_20",
+    },    
+    "s_40":{
+        "v_10":"Err_80",
+        "v_20":"s_40",
+        "v_30":"Err_90",
+        "v_40":"END",
+        "v_50":"Err_20",
+   }    
 }
 
 # define event code
@@ -29,10 +61,22 @@ while i < len(real_number) + 1:
         event = "v_40"
 
     print("i:[" + str(i) +"] and event:[" + event + "]")
-    
+
+    # define new state
+    state = fsm_matrix[current_state][event]
+    previous_state = current_state
+    current_state = state
+
+    if current_state in fsm_matrix:
+        if current_state == "s_20":
+            integer_part += real_number[i]
+        if current_state == "s_40":
+            decimal_part += real_number[i]
+
+
     i += 1
 
-#new_state = fsm_matrix[current_state][event]
+print("integer_part:[" + integer_part +"] and decimal_part:[" + decimal_part + "]")
 
 
 
