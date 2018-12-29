06461ef500a6714e52ba2063cbf1eb903db22b9b
Val Rakitine <val@natmatch.com>
2018-12-28 19:51:40 -0500

    defined event code

--- fsm_p_01_main.py
+++ fsm_p_01_main.py
@@ -1,6 +1,6 @@
 import json
 # init block
-real_number = str("+38.7")
+real_number = str("+38t.7")
 integer_part = str("")
 decimal_part = str("")
 current_state = str("s_00")
@@ -12,25 +12,27 @@ fsm_matrix = {
 }
 
 # define event code
-event = str("s_50")
 i = 0
-while i < len(real_number):
-    event = str("unknown")
+while i < len(real_number) + 1:
+    event = "s_50"
+    
+    if i < len(real_number):
+        if real_number[i] in ["+","-"]:
+            event = "v_10"
 
-    if real_number[i] in ["+","-"]:
-         event = "v_10"
-        break
-    if real_number[i] in ["0","1","2","3","4","5","6","7","8","9"]:
-        event = "v_20"
-        break
-    if real_number[i] in ["."]:
-        event = "v_30"
-        break
+        if real_number[i] in ["0","1","2","3","4","5","6","7","8","9"]:
+            event = "v_20"
 
+        if real_number[i] in ["."]:
+            event = "v_30"
+    if i == len(real_number):
+        event = "v_40"
+
+    print("i:[" + str(i) +"] and event:[" + event + "]")
     
     i += 1
 
 #new_state = fsm_matrix[current_state][event]
 
 
-print("i:[" + str(i) +"] and event:[" + event + "]")
+
